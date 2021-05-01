3609ee668ec48cdf4cd6ed20cc166b4fd7ae368e
taejin <taejin5314@gmail.com>
2021-05-01 18:00:40 -0400

    making character class

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ canvas.width = window.innerWidth;
 const images = {};
 images.player = new Image();
 images.player.src = 'Character.png';
+const characterActions = ['up', 'top right', 'right', 'down right', 'down', 'jump'];
+
+class Character {
+  constructor() {
+    this.width = 103.0625;
+    this.height = 113.125;
+    this.frameX = 3;
+    this.frameY = 3;
+    this.x = 0;
+    this.y = 0;
+    this.speed = (Math.random() * 1.5) + 3.5;
+
+  }
+}
 
 const playerWidth = 103.0625;
 const playerHeight = 113.125;
